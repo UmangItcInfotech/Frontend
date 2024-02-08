0c9becec80525d3a4cf9d49cab46641352a4fb50
UmangItcInfotech <148256049+UmangItcInfotech@users.noreply.github.com>
2024-02-09 01:26:56 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    userBaseUrl: 'http://gaurav-user.itcblogs.xyz',
-    blogBaseUrl: 'http://gaurav-blog.itcblogs.xyz',
-    commentBaseUrl: 'http://gaurav-comment.itcblogs.xyz',
-    imageBaseUrl: 'http://gaurav-image.itcblogs.xyz'
+    userBaseUrl: 'http://umang-user.itcblogs.xyz',
+    blogBaseUrl: 'http://umang-blog.itcblogs.xyz',
+    commentBaseUrl: 'http://umang-comment.itcblogs.xyz',
+    imageBaseUrl: 'http://umang-image.itcblogs.xyz'
   };
